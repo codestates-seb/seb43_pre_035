00b047eaf511f91046ad3f08e886c8499fa212b8
yeonssu <dustn7197@naver.com>
2023-04-14 15:50:55 +0900

    add: create project

---
+++ server/qna-server/src/main/java/com/hoot/QnaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaServerApplication.class, args);
+	}
+
+}
