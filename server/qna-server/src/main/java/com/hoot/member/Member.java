92ab3361e0ab0444b1658d4b89c6764c2b748f65
yeonssu <dustn7197@naver.com>
2023-04-14 16:29:06 +0900

    create: 엔티티 생성

--- server/qna-server/src/main/java/com/hoot/member/Member.java
+++ server/qna-server/src/main/java/com/hoot/member/Member.java
@@ -1,2 +1,32 @@
-package com.hoot.answer;public class Member {
+package com.hoot.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long memberId;
+
+    public String email;
+
+    public String password;
+
+    public String name;
+
+    public String displayName;
+
+    public LocalDateTime createdDate = LocalDateTime.now();
+
+    public LocalDateTime updateDate;
+
 }
