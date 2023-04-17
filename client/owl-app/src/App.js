85f48352280d4966aa5393c7f0b40650425e6572
Zeereoreo <wltjrcjf12@naver.com>
2023-04-17 17:44:10 +0900

    feat : TopnavTemplate 추가

--- client/owl-app/src/App.js
+++ client/owl-app/src/App.js
@@ -13,7 +13,6 @@ function App() {
       <SideNav />
       <Threads />
     </Router>
-
   );
 }
 
--- client/owl-app/src/components/TopNav.js
+++ client/owl-app/src/components/TopNav.js
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+
+const TopnavTemplate = styled.div`
+    padding : 20px;
+    width: 100%;
+    height: 100px;
+`
+
 const TopNav = () => {
     return (
-        <>
+        <TopnavTemplate>
                  <p> This is the Top Navigation Pane. </p>
                  <Link to = '/'>Home</Link>
                  -
                  <Link to = '/login'>Login</Link>
                  -
                  <Link to = '/signup'>SignUp</Link>
-        </>
+        </TopnavTemplate>
 
 
     )
