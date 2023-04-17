7c0580686a9331e90b9b31cec34de46422baa534
sung <sunga.jlh@gmail.com>
2023-04-17 17:30:57 +0900

    Set-up: 상단 내비게이션 라우터 링크 껍데기 셋업

--- client/owl-app/src/components/TopNav.js
+++ client/owl-app/src/components/TopNav.js
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
 const TopNav = () => {
     return (
-        <p> This is the Top Navigation Pane. </p>
+        <>
+                 <p> This is the Top Navigation Pane. </p>
+                 <Link to = '/'>Home</Link>
+                 -
+                 <Link to = '/login'>Login</Link>
+                 -
+                 <Link to = '/signup'>SignUp</Link>
+        </>
+
+
     )
 }
 
