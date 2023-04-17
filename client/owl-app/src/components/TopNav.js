f869892fa9588025048f291881aa7918d6164ca4
Zeereoreo <wltjrcjf12@naver.com>
2023-04-17 17:52:59 +0900

    feat : Topnav Input 구현

--- client/owl-app/src/components/TopNav.js
+++ client/owl-app/src/components/TopNav.js
@@ -5,20 +5,27 @@ const TopnavTemplate = styled.div`
     padding : 20px;
     width: 100%;
     height: 100px;
+    display: flex;
+    justify-content: space-around;
+`
+
+const TopnavInput = styled.input`
+    padding: 20px;
+    width: 840px;
+    height: 0.01rem;
 `
 
 const TopNav = () => {
     return (
         <TopnavTemplate>
-                 <p> This is the Top Navigation Pane. </p>
+                 <span> Logo </span>
+                 <TopnavInput type='text'></TopnavInput>
                  <Link to = '/'>Home</Link>
                  -
                  <Link to = '/login'>Login</Link>
                  -
                  <Link to = '/signup'>SignUp</Link>
         </TopnavTemplate>
-
-
     )
 }
 
