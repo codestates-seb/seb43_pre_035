1f4edcfedc5a1d24a17982788bdfd7a5e9c8b6e4
yeonssu <dustn7197@naver.com>
2023-04-18 01:03:40 +0900

    add : MemberRepository에 @Repository 추가

--- server/qna-server/src/main/java/com/hoot/member/MemberRepository.java
+++ server/qna-server/src/main/java/com/hoot/member/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.hoot.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
 }
