d42f9dee9e7feb581d82763892b5e4b5126eda2f
sung <sunga.jlh@gmail.com>
2023-04-17 18:09:27 +0900

    added styled components in TopNav

--- client/owl-app/src/components/TopNav.js
+++ client/owl-app/src/components/TopNav.js
@@ -1,9 +1,28 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const TopnavTemplate = styled.div`
+    padding : 20px;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: space-around;
+`
+
+const TopnavInput = styled.input`
+    padding: 20px;
+    width: 840px;
+    height: 0.01rem;
+`
 
 const TopNav = () => {
     return (
         <div>
                  <p>TOP NAV</p>
+                 <TopnavTemplate>
+                    <span> Logo </span>
+                    <TopnavInput type='text'></TopnavInput>
+                </TopnavTemplate>
                  <Link to = '/'>Home</Link>
                  -
                  <Link to = '/login'>Login</Link>
