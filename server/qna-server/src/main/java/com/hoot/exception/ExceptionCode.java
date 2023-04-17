ce625530a6edfa98987ab45609018d4afaa5245d
justgo0122 <rhdbrkd2@naver.com>
2023-04-17 22:43:19 +0900

    add : ExceptionCode 생성 및 REPLY 예외 일부 구현

---
+++ server/qna-server/src/main/java/com/hoot/exception/ExceptionCode.java
@@ -0,0 +1,19 @@
+package com.hoot.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    QUESTION_REPLY_NOT_FOUND(404, "Question Comment Not Found"),
+    ANSWER_REPLY_NOT_FOUND(404, "Answer Comment Not Found");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
