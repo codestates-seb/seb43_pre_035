15836bacaa2c7638970b00558f7689808822fce0
yeonssu <dustn7197@naver.com>
2023-04-18 00:34:06 +0900

    add : Jwt 인증 로직 관련 클래스 생성
    JwtAuthenticationFilter 클래스 생성 및 JwtTokenProvider 클래스 생성

---
+++ server/qna-server/src/main/java/com/hoot/security/JwtTokenProvider.java
@@ -0,0 +1,98 @@
+package com.hoot.security;
+
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    private final Logger LOGGER = LoggerFactory.getLogger(JwtTokenProvider.class);
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.key}")
+    private String secretKey;
+    byte[] secretKeyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+    Key key = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS256.getValue());
+
+    @Value("${jwt.access-token-expiration-minutes}")
+    private long accessTokenExpirationMinutes;
+
+    // Security 에 대해 인코딩 수행
+    @PostConstruct
+    protected void init() {
+        LOGGER.info("[init] JwtTokenProvider 내 secretKey 초기화 시작");
+        secretKey = Base64.getEncoder().encodeToString(secretKeyBytes);
+        LOGGER.info("[init] JwtTokenProvider 내 secretKey 초기화 완료");
+    }
+
+    // JWT 토큰 생성
+    public String createToken(String email, List<String> roles) {
+        LOGGER.info("[createToken] 토큰 생성 시작");
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("roles", roles);
+
+        Date now = new Date();
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + accessTokenExpirationMinutes))
+                .signWith(key)
+                .compact();
+
+        LOGGER.info("[createToken] 토큰 생성 완료");
+        return token;
+    }
+
+    // JWT 토큰을 통한 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        LOGGER.info("[getAuthentication] 토큰 인증 정보 조회 시작");
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserEmail(token));
+        LOGGER.info("[getAuthentication] 토큰 인증 정보 조회 완료, UserDetails UserName : {}", userDetails.getUsername());
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // JWT 토큰에서 회원 구별 정보 추출
+    private String getUserEmail(String token) {
+        LOGGER.info("[getUsername] 토큰 기반 회원 구별 정보 추출");
+        String info = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+        LOGGER.info("[getUsername] 토큰 기반 회원 구별 정보 추출 완료, info : {}", info);
+
+        return info;
+    }
+
+    // HTTP Request Header 에 설정된 Token 값을 가져옴
+    public String resolveToken(HttpServletRequest request) {
+        LOGGER.info("[resolveToken] HTTP Header 에서 Token 값 추출");
+        return request.getHeader("Authorization");
+    }
+
+    // JWT 토큰의 유효성 + 만료일 체크
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            LOGGER.info("[validateToken] 토큰 유효 체크 완료");
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (JwtException e) {
+            LOGGER.info("[validateToken] 토큰 유효 체크 예외 발생");
+            return false;
+        }
+    }
+}
