49dc40d7bcd1fe4b32f7c2987e6086982a5aa63a
Gorden-Dosi <115977201+Gorden-Dosi@users.noreply.github.com>
2023-04-17 19:54:01 +0900

    add form SideNav
    add form SideNav

--- client/owl-app/src/components/SideNav.js
+++ client/owl-app/src/components/SideNav.js
@@ -1,7 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const SideNavContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 200px;
+    left: 50px;
+    width: 200px;
+    height: 150px;
+    background-color: #4F3F48;
+    border-radius: 2px;
+    padding: 10px;
+    box-sizing: border-box;
+    float: left;
+`;
+
+const SideNavItem = styled.div`
+    display: block;
+    height: 25px;
+    padding: 8px;
+    color: #fff;
+    text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+
 const SideNav = () => {
-    return (
-        <p> This is the Side Nav. </p>
-    )
-}
+  return (
+    <SideNavContainer>
+      <SideNavItem>Home</SideNavItem>
+      <SideNavItem>Public</SideNavItem>
+      <SideNavItem>Tags</SideNavItem>
+    </SideNavContainer>
+  );
+};
 
 export default SideNav;
