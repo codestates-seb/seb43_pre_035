3636a7ab7aeb36735f351498b55d67a0a45a0e30
Zeereoreo <wltjrcjf12@naver.com>
2023-04-18 20:30:16 +0900

    feat : Topnav 전면 수정

--- client/owl-app/src/App.js
+++ client/owl-app/src/App.js
@@ -2,27 +2,21 @@ import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //import components
-import TopNav from './components/TopNav';
 import SideNav from './components/SideNav';
 import Threads from './components/Threads';
 //import pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import TopNavlogged from './components/TopNavlogged'
+import Header from './components/header/Header';
 
 function App() {
 
-  const [nav,setLogednav] = useState(false)
-
-  const handleClicknav = () => {
-    setLogednav(!nav)
-  } 
 
   return (
-    
+
     <Router>
-      <button onClick={handleClicknav}>{nav ? <TopNav /> : <TopNavlogged/> }</button>
+      <Header/>
       <Routes>
             <Route path ="/" element = {<Home />} />
             <Route path ="/login" element = {<Login />} />
@@ -30,9 +24,10 @@ function App() {
                  {/* <p> This is the Top Navigation Pane. </p>
                  <Link to = '/'>Home</Link>
                  <Link to = '/login'>Login</Link>
-                 <Link to = '/signup'>SignUp</Link> */}
+                <Link to = '/signup'>SignUp</Link> */}
         </Routes>
     </Router>
+
 
   );
 }
