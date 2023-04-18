cd023d81b0ac8c974a3bb7f11f86a4b39ff0ea38
Zeereoreo <wltjrcjf12@naver.com>
2023-04-18 19:12:43 +0900

    feat : comment component 목업 기능 구현

--- client/owl-app/src/pages/Question/AddComment.js
+++ client/owl-app/src/pages/Question/AddComment.js
@@ -1,4 +1,6 @@
 import styled, {css} from "styled-components";
+import {useState} from 'react'
+import CommentInput from "./CommentInput"
 
 const AddCommentWrap = styled.div`
     padding: 10px;
@@ -10,34 +12,6 @@ const AddCommentWrap = styled.div`
     }
 `
 
-const CommentInputWrap = styled.div`
-    padding: 10px;
-    width: 750px;
-    height: 90px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-
-const CommentInput = styled.input`
-    padding: 10px;
-    width: 700px;
-    height: 40px;
-    border: 1px solid #E7CC8F;
-    background-color: #493E3B;
-    .placeholder{
-        color : white;
-    }
-`
-
-const CreatButton = styled.button`
-    padding: 10px;
-    width: 100px;
-    height: 30px;
-    color: white;
-    border: 1px solid white;
-    background-color: #BF8B67;
-`
 
 const CommentCreatWrap = styled.div`
     padding: 10px;
@@ -67,18 +41,20 @@ const CommentDate = styled.div`
 
 const AddComment = () => {
 
+    const [comment,setComment] = useState(true)
+
+    const commentHandleClick = ()=>{
+        setComment(!comment)
+    }
+
     return (
         <>
-        <AddCommentWrap>Add a AddComment</AddCommentWrap>
-        <CommentInputWrap>
-            <CommentInput type="text" placeholder="Comment를 달아주세요"/>
-            <CreatButton>작성하기</CreatButton>
+        <AddCommentWrap onClick={commentHandleClick}>{setComment ? <div>Add a Comment </div> :<CommentInput/>}</AddCommentWrap>
             <CommentCreatWrap>
                 <CommentDetail>Comment</CommentDetail>
                 <CommentUser>닉네임</CommentUser>
                 <CommentDate>0분 전 작성</CommentDate>
             </CommentCreatWrap>
-        </CommentInputWrap>
         </>
     )
 }
