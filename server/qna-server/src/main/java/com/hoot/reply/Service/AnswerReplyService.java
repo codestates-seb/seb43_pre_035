7c34daa3642c8ed40633a4cb9d9b40b6b9fbeb98
justgo0122 <rhdbrkd2@naver.com>
2023-04-18 11:22:42 +0900

    add : AnswerReplyService 구현

---
+++ server/qna-server/src/main/java/com/hoot/reply/Service/AnswerReplyService.java
@@ -0,0 +1,45 @@
+package com.hoot.reply.Service;
+
+import com.hoot.exception.BusinessLogicException;
+import com.hoot.exception.ExceptionCode;
+import com.hoot.reply.Entity.AnswerReply;
+import com.hoot.reply.Repository.AnswerReplyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AnswerReplyService {
+
+    private final AnswerReplyRepository answerReplyRepository;
+
+    public AnswerReplyService(AnswerReplyRepository answerReplyRepository) {
+        this.answerReplyRepository = answerReplyRepository;
+    }
+
+    public AnswerReply createAnswerReply(AnswerReply answerReply) {
+        return answerReplyRepository.save(answerReply);
+    }
+
+    public AnswerReply updateAnswerReply(AnswerReply answerReply) {
+        AnswerReply findAnswerReply
+                = findVerifiedAnswerReply(answerReply.getAnswerReplyId());
+
+        Optional.ofNullable(answerReply.getContent())
+                .ifPresent(content -> findAnswerReply.setContent(content));
+
+        return answerReplyRepository.save(findAnswerReply);
+    }
+
+    public void deleteAnswerReply(Long answerReplyId) {
+        AnswerReply answerReply = findVerifiedAnswerReply(answerReplyId);
+        answerReplyRepository.delete(answerReply);
+    }
+
+    private AnswerReply findVerifiedAnswerReply(Long answerReplyId) {
+        Optional<AnswerReply> optionalAnswerReply = answerReplyRepository.findById(answerReplyId);
+        AnswerReply answerReply
+                = optionalAnswerReply.orElseThrow(() -> new BusinessLogicException(ExceptionCode.ANSWER_REPLY_NOT_FOUND));
+        return answerReply;
+    }
+}
