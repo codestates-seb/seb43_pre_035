b5a5d6b3e498a6691a7ca245f121fe4280cae1b5
sung <sunga.jlh@gmail.com>
2023-04-18 15:06:52 +0900

    test: ngrok get/post test success

--- client/owl-app/src/tests/ngroktest.js
+++ client/owl-app/src/tests/ngroktest.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
-const url = "https://koreanjson.com/users";
-
+// const url = "https://koreanjson.com/users";
+// const url = "https://6ff2-124-61-224-204.jp.ngrok.io/hello";
+const url = "https://d9ce-124-61-224-204.ngrok-free.app/hello/world";
+const url_signup = "https://d9ce-124-61-224-204.ngrok-free.app/users/signup";
 
 axios.interceptors.request.use(config => {
     // log a message before any HTTP request is sent
@@ -12,6 +14,13 @@ axios.interceptors.request.use(config => {
 axios
   .get(url)
   .then((response) => console.log(response.data));
+
+axios
+  .post(url_signup, {email: "sunga.jlh@gmail.com", password: "1111"})
+  .then((response) => {
+    const data = response.data;
+    console.log(data);
+  })
 
 
 // axios
