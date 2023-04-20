b0d47cf21e609cfa28c6c31a9d17da075bde7113
yeonssu <dustn7197@naver.com>
2023-04-20 09:48:14 +0900

    fix : member 에서 answer, question 매핑

--- server/qna-server/src/main/java/com/hoot/answer/repository/AnswerRepository.java
+++ server/qna-server/src/main/java/com/hoot/answer/repository/AnswerRepository.java
@@ -1,9 +1,13 @@
 package com.hoot.answer.repository;
 
 import com.hoot.answer.entity.Answer;
+import com.hoot.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository  extends JpaRepository<Answer,Long> {
+    List<Answer> findByMember(Member member);
 }
