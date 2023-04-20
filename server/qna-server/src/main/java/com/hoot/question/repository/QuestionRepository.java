3779f680240e18ddb5d41981b987cc19f8c5220c
jyoungGrace <jyjeon.grace@gmail.com>
2023-04-20 12:01:53 +0900

    멤버 파인드 추가로 인해 merge

--- server/qna-server/src/main/java/com/hoot/question/repository/QuestionRepository.java
+++ server/qna-server/src/main/java/com/hoot/question/repository/QuestionRepository.java
@@ -9,9 +9,7 @@ import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository <Question, Long>{
-<<<<<<< HEAD
 	List<Question> findByTitleContaining(String keyword);
-=======
-    List<Question> findByMember(Member member);
->>>>>>> 357bd9b9ee82137716f38445c1c30fd37db82dd3
+  List<Question> findByMember(Member member);
+
 }
