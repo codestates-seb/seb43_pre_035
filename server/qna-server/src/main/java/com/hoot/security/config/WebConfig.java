c4d084fdbf27b991e19cf6d631500fc22ce63d7f
yeonssu <dustn7197@naver.com>
2023-04-21 20:44:05 +0900

    fix : Cors 모든 Origin 허용으로 변경

--- server/qna-server/src/main/java/com/hoot/security/config/WebConfig.java
+++ server/qna-server/src/main/java/com/hoot/security/config/WebConfig.java
@@ -9,10 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/users/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization");
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
