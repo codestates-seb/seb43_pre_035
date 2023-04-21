3e9a51a5a2f1089411b843244387c7210613ce87
justgo0122 <rhdbrkd2@naver.com>
2023-04-21 14:32:14 +0900

    refactor : QuestionReply API 수정

--- server/qna-server/src/main/java/com/hoot/reply/service/QuestionReplyService.java
+++ server/qna-server/src/main/java/com/hoot/reply/service/QuestionReplyService.java
@@ -5,6 +5,8 @@ import com.hoot.exception.ExceptionCode;
 import com.hoot.member.Member;
 import com.hoot.member.MemberRepository;
 import com.hoot.member.MemberService;
+import com.hoot.question.Question;
+import com.hoot.question.repository.QuestionRepository;
 import com.hoot.reply.entity.QuestionReply;
 import com.hoot.reply.repository.QuestionReplyRepository;
 import com.hoot.security.UserDetailsImpl;
@@ -17,25 +19,28 @@ public class QuestionReplyService {
     private final QuestionReplyRepository questionReplyRepository;
     private final MemberRepository memberRepository;
     private final MemberService memberService;
+    private final QuestionRepository questionRepository;
 
-    public QuestionReplyService(QuestionReplyRepository questionReplyRepository, MemberRepository memberRepository, MemberService memberService) {
+    public QuestionReplyService(QuestionReplyRepository questionReplyRepository, MemberRepository memberRepository, MemberService memberService, QuestionRepository questionRepository) {
         this.questionReplyRepository = questionReplyRepository;
         this.memberRepository = memberRepository;
         this.memberService = memberService;
+        this.questionRepository = questionRepository;
     }
 
-    public QuestionReply createQuestionReply(UserDetailsImpl user, QuestionReply questionReply) {
+    public QuestionReply createQuestionReply(UserDetailsImpl user,Long questionId, QuestionReply questionReply) {
         replaceQuestionReplyMemberToLogInMember(user, questionReply);
+        replaceQuestion(questionReply,questionId);
         return questionReplyRepository.save(questionReply);
     }
 
 
 
     public QuestionReply updateQuestionReply(UserDetailsImpl user,QuestionReply questionReply) {
-        memberService.verifyLogInMemberMatchesMember(user.getMemberId(), questionReply.getMember().getMemberId());
-
         QuestionReply findQuestionReply
                 = findVerifiedQuestionReply(questionReply.getQuestionReplyId());
+
+        memberService.verifyLogInMemberMatchesMember(user.getMemberId(), findQuestionReply.getMember().getMemberId());
 
         Optional.ofNullable(questionReply.getContent())
                 .ifPresent(content -> findQuestionReply.setContent(content));
@@ -63,6 +68,12 @@ public class QuestionReplyService {
         Optional<Member> optionalMember = memberRepository.findById(user.getMemberId());
         Member member = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
         questionReply.setMember(member);
+    }
+
+    private void replaceQuestion(QuestionReply questionReply,Long questionId) {
+        Optional<Question> optionalQuestion = questionRepository.findById(questionId);
+        Question question = optionalQuestion.orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUESTION_NOT_FOUND));
+        questionReply.setQuestion(question);
     }
 
 }
