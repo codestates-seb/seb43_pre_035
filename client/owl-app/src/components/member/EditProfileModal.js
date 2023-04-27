bca9e3922432c4c873047d7c05b746dde62dbd2c
Gorden-Dosi <gosangmin1@gmail.com>
2023-04-27 19:27:58 +0900

    feat : 모달 유효성 기능 및 수정 기능 안정화

--- client/owl-app/src/components/member/EditProfileModal.js
+++ client/owl-app/src/components/member/EditProfileModal.js
@@ -1,265 +1,297 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { axiosAuth2 } from '../../utils/axiosConfig';
 
 
 const ModalBackdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+  `;
 
 const ModalWrapper = styled.div`
-  background-color: white;
-  padding: 20px;
-  width: 300px;
-  border-radius: 5px;
-  background-color: rgba(157, 83, 83, 0.8);
-  border: 2px solid #9D5353;
-`;
+    background-color: white;
+    padding: 20px;
+    width: 300px;
+    border-radius: 5px;
+    background-color: rgba(157, 83, 83, 0.8);
+    border: 2px solid #9D5353;
+  `;
 
 const CloseButton = styled.button`
-  background-color: transparent;
-  border: none;
-  font-size: 24px;
-  cursor: pointer;
-  position: absolute;
-  right: 10px;
-  top: 10px;
-`;
+    background-color: transparent;
+    border: none;
+    font-size: 24px;
+    cursor: pointer;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  `;
 
 const EditProfileForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
 
 const FormField = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin-bottom: 10px;
-`;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 10px;
+  `;
 
 const EmailText = styled.label`
-  margin-bottom: 5px;
-  font-size: 20px;
-  font-weight: bold;
-`;
+    margin-bottom: 5px;
+    font-size: 20px;
+    font-weight: bold;
+  `;
 
 const Label = styled.label`
-  margin-bottom: 5px;
-`;
+    margin-bottom: 5px;
+  `;
 
 const Input = styled.input`
-  height: 30px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 5px;
-`;
+    height: 30px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 5px;
+  `;
 
 const AlertText = styled.div`
-  color: red;
-  font-size: 12px;
-  margin-top: 5px;
-`;
+    color: red;
+    font-size: 12px;
+    margin-top: 5px;
+  `;
 
 const SubmitButton = styled.button`
+    background-color: #000;
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 10px 20px;
+    border-radius: 5px;
+    border: none;
+    margin-top: 10px;
+  `;
+
+const DeleteButton = styled.button`
   background-color: #000;
   color: #fff;
-  font-size: 16px;
-  font-weight: bold;
-  padding: 10px 20px;
+  font-size: 13px;
+  padding: 5px 10px;
   border-radius: 5px;
   border: none;
   margin-top: 10px;
-`;
-
-const DeleteButton = styled.button`
-background-color: #000;
-color: #fff;
-font-size: 13px;
-padding: 5px 10px;
-border-radius: 5px;
-border: none;
-margin-top: 10px;
-`;
+  `;
 
 const validatePassword = (password) => {
-    const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,16}$/;
-    return re.test(password);
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,16}$/;
+  return re.test(password);
 };
 
 const validateDisplayName = (displayName) => {
-    const re = /^[a-zA-Z0-9가-힣]{2,15}$/;
-    return re.test(displayName);
+  const re = /^[a-zA-Z0-9가-힣]{2,15}$/;
+  return re.test(displayName);
 };
 
 const EditProfileModal = ({ isOpen, onRequestClose, userFullInfo: propsUserFullInfo }) => {
-    const [name, setName] = useState(propsUserFullInfo?.name);
-    const [displayName, setDisplayName] = useState(propsUserFullInfo?.displayName);
-    const [password, setPassword] = useState('');
-    const [member_id, setMember_id] = useState(propsUserFullInfo);
-    const [displayNameAlert, setDisplayNameAlert] = useState(false);
-    const [passwordAlert, setPasswordAlert] = useState(false);
-    const history = useNavigate();
+  const [name, setName] = useState(propsUserFullInfo?.name);
+  const [displayName, setDisplayName] = useState(propsUserFullInfo?.displayName);
+  const [password, setPassword] = useState('');
+  const [member_id, setMember_id] = useState(propsUserFullInfo);
+  const [displayNameAlert, setDisplayNameAlert] = useState(false);
+  const [passwordAlert, setPasswordAlert] = useState(false);
+  const history = useNavigate();
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isDisplayNameValid, setIsDisplayNameValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [isPasswordEmpty, setIsPasswordEmpty] = useState(true);
 
-    // console.log("displayname:",propsUserFullInfo?.displayName)
-    // console.log("displayname2:",displayName)
+  // console.log("displayname:",propsUserFullInfo?.displayName)
+  // console.log("displayname2:",displayName)
 
-    
-    
-    const handleNameChange = (event) => {
-      setName(event.target.value);
-    };
-  
-    const handleDisplayNameChange = (event) => {
-      setDisplayName(event.target.value);
-  
-      if (!validateDisplayName(event.target.value)) {
-        setDisplayNameAlert(true);
-      } else {
-        setDisplayNameAlert(false);
-      }
-    };
-  
-    const handlePasswordChange = (event) => {
-      setPassword(event.target.value);
-      if (!validatePassword(event.target.value)) {
-        setPasswordAlert(true);
-      } else {
-        setPasswordAlert(false);
-      }
-    };
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      const updatedData = {};
-  
-      if (name !== propsUserFullInfo?.name) {
-        updatedData.name = name;
-      }
-  
-      if (displayName !== propsUserFullInfo?.displayName) {
-        if (!validateDisplayName(displayName)) {
-          alert('디스플레이 네임은 특수문자를 제외한 2~15자리여야 합니다.');
-          return;
-        }
-        updatedData.displayName = displayName;
-      }
-  
-      if (password) {
-        if (!validatePassword(password)) {
-          alert('비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.');
-          return;
-        }
-        updatedData.password = password;
-      }
-  
-      if (Object.keys(updatedData).length === 0) {
-        alert('변경된 정보가 없습니다.');
+  useEffect(() => {
+    if (!isOpen) {
+      setName(propsUserFullInfo?.name);
+      setDisplayName(propsUserFullInfo?.displayName);
+      setPassword('');
+      setMember_id(propsUserFullInfo);
+      setDisplayNameAlert(false);
+      setPasswordAlert(false);
+      setIsNameValid(true);
+      setIsDisplayNameValid(true);
+      setIsPasswordValid(false);
+      setIsPasswordEmpty(true);
+    }
+  }, [isOpen, propsUserFullInfo]);
+
+  useEffect(() => {
+    setName(propsUserFullInfo?.name);
+    setDisplayName(propsUserFullInfo?.displayName);
+    setMember_id(propsUserFullInfo);
+  }, [propsUserFullInfo]);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    setIsNameValid(event.target.value.length > 0);
+  };
+
+  const handleDisplayNameChange = (event) => {
+    setDisplayName(event.target.value);
+
+    if (!validateDisplayName(event.target.value)) {
+      setDisplayNameAlert(true);
+      setIsDisplayNameValid(false);
+    } else {
+      setDisplayNameAlert(false);
+      setIsDisplayNameValid(true);
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setIsPasswordEmpty(event.target.value === '');
+    if (!validatePassword(event.target.value)) {
+      setPasswordAlert(true);
+      setIsPasswordValid(false);
+    } else {
+      setPasswordAlert(false);
+      setIsPasswordValid(true);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const updatedData = {};
+
+    if (name !== propsUserFullInfo?.name) {
+      updatedData.name = name;
+    }
+
+    if (displayName !== propsUserFullInfo?.displayName) {
+      if (!validateDisplayName(displayName)) {
+        alert('디스플레이 네임은 특수문자를 제외한 2~15자리여야 합니다.');
         return;
       }
+      updatedData.displayName = displayName;
+    }
 
-      updateProfile(updatedData);
-    };
+    if (password) {
+      if (!validatePassword(password)) {
+        alert('비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.');
+        return;
+      }
+      updatedData.password = password;
+    }
 
-    const updateProfile = async (updatedData) => {
-        try {
-          const response = await axiosAuth2.patch(
-            `${process.env.REACT_APP_URL_NGROKTEST}/users/${propsUserFullInfo?.memberId}`,
-            updatedData
-          );
-          console.log(updatedData)
-          setMember_id(response.data);
-          onRequestClose();
-          alert('회원 정보가 수정되었습니다.');
-          history(0)
-        } catch (error) {
-          console.error('Error updating profile:', error);
-          alert('회원 정보 수정 중 오류가 발생했습니다. 다시 시도해주세요.');
-        }
-      };
-      
-      const deleteProfile = async () => {
-        if (window.confirm('정말로 회원 탈퇴를 하시겠습니까?')) {
-          try {
-            await axiosAuth2.delete(
-              `${process.env.REACT_APP_URL_NGROKTEST}/users/${propsUserFullInfo.memberId}`
-            );
-            alert('회원 탈퇴가 완료되었습니다.');
-            onRequestClose();
-            history.push('/');
-          } catch (error) {
-            console.error('Error deleting profile:', error);
-            alert('회원 탈퇴 중 오류가 발생했습니다. 다시 시도해주세요.');
-          }
-        }
-      };
-      
+    if (Object.keys(updatedData).length === 0) {
+      alert('변경된 정보가 없습니다.');
+      return;
+    }
 
-    return (
-        <>
-            {isOpen && (
-                <ModalBackdrop onClick={onRequestClose}>
-                    <ModalWrapper onClick={(e) => e.stopPropagation()}>
-                        <CloseButton onClick={onRequestClose}>&times;</CloseButton>
-                        <EditProfileForm onSubmit={handleSubmit}>
-                            <EmailText htmlFor="Email">{propsUserFullInfo?.email}</EmailText>
-                            <DeleteButton type="button" onClick={deleteProfile}>Delete Profile</DeleteButton>
-                            <FormField>
-                                <Label htmlFor="name">Name</Label>
-                                <Input
-                                    type="text"
-                                    id="name"
-                                    value={name}
-                                    onChange={handleNameChange}
-                                />
-                            </FormField>
-                            <FormField>
-                                <Label htmlFor="displayName">Display Name</Label>
-                                <Input
-                                    type="text"
-                                    id="displayName"
-                                    value={displayName}
-                                    onChange={handleDisplayNameChange}
-                                />
-                                {displayNameAlert && (
-                                    <AlertText>
-                                        디스플레이 네임은 특수문자를 제외한 2~15자리여야 합니다.
-                                    </AlertText>
-                                )}
-                            </FormField>
-                            <FormField>
-                                <Label htmlFor="password">Password</Label>
-                                <Input
-                                    type="password"
-                                    id="password"
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                />
-                                {passwordAlert && (
-                                    <AlertText>
-                                        비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.
-                                    </AlertText>
-                                )}
-                            </FormField>
-                            <SubmitButton type="submit">Save Profile</SubmitButton>
-                        </EditProfileForm>
-                    </ModalWrapper>
-                </ModalBackdrop>
-            )}
-        </>
-    );
+    updateProfile(updatedData);
+  };
+
+  const updateProfile = async (updatedData) => {
+    try {
+      const response = await axiosAuth2.patch(
+        `${process.env.REACT_APP_URL_NGROKTEST}/users/${propsUserFullInfo?.memberId}`,
+        updatedData
+      );
+      console.log(updatedData)
+      setMember_id(response.data);
+      onRequestClose();
+      alert('회원 정보가 수정되었습니다.');
+      history(0)
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      alert('회원 정보 수정 중 오류가 발생했습니다. 다시 시도해주세요.');
+    }
+  };
+
+  const deleteProfile = async () => {
+    if (window.confirm('정말로 회원 탈퇴를 하시겠습니까?')) {
+      try {
+        await axiosAuth2.delete(
+          `${process.env.REACT_APP_URL_NGROKTEST}/users/${propsUserFullInfo.memberId}`
+        );
+        alert('회원 탈퇴가 완료되었습니다.');
+        onRequestClose();
+        history.push('/');
+      } catch (error) {
+        console.error('Error deleting profile:', error);
+        alert('회원 탈퇴 중 오류가 발생했습니다. 다시 시도해주세요.');
+      }
+    }
+  };
+
+
+  return (
+    <>
+      {isOpen && (
+        <ModalBackdrop onClick={onRequestClose}>
+          <ModalWrapper onClick={(e) => e.stopPropagation()}>
+            <CloseButton onClick={onRequestClose}>&times;</CloseButton>
+            <EditProfileForm onSubmit={handleSubmit}>
+              <EmailText htmlFor="Email">{propsUserFullInfo?.email}</EmailText>
+              <DeleteButton type="button" onClick={deleteProfile}>Delete Profile</DeleteButton>
+              <FormField>
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+              </FormField>
+              <FormField>
+                <Label htmlFor="displayName">Display Name</Label>
+                <Input
+                  type="text"
+                  id="displayName"
+                  value={displayName}
+                  onChange={handleDisplayNameChange}
+                />
+                {displayNameAlert && (
+                  <AlertText>
+                    디스플레이 네임은 특수문자를 제외한 2~15자리여야 합니다.
+                  </AlertText>
+                )}
+              </FormField>
+              <FormField>
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                {passwordAlert && (
+                  <AlertText>
+                    비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 포함해야 합니다.
+                  </AlertText>
+                )}
+              </FormField>
+              <SubmitButton
+                type="submit"
+                disabled={!isNameValid || !isDisplayNameValid || (!isPasswordValid && !isPasswordEmpty)}>
+                Save Profile</SubmitButton>
+            </EditProfileForm>
+          </ModalWrapper>
+        </ModalBackdrop>
+      )}
+    </>
+  );
 };
 
 export default EditProfileModal;
--- client/owl-app/src/components/member/MyPage.js
+++ client/owl-app/src/components/member/MyPage.js
@@ -7,148 +7,153 @@ import { axiosAuth2 } from '../../utils/axiosConfig';
 // import useFetch from '../../utils/useFetch'; 
 
 const MypageWrap = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `
 
 const MypageBlock = styled.div`
-    margin-top: 10px;
-    padding: 20px;
-    height: 95vh;
-    width: 700px;
-    background-color: #BF8B67;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    border-radius: 5px;
-`
+        margin-top: 10px;
+        padding: 20px;
+        height: 95vh;
+        width: 700px;
+        background-color: #BF8B67;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        border-radius: 5px;
+    `
 
 const UserBlock = styled.div`
-    position: relative;
-    display: flex;
-    flex-direction: center;
-    margin-bottom:10px;
-    width: 700px;
-    height: 100px;
-    align-items: center;
-`
+        position: relative;
+        display: flex;
+        flex-direction: center;
+        margin-bottom:10px;
+        width: 700px;
+        height: 100px;
+        align-items: center;
+    `
 
 const UserAvatar = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-`
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    `
 
 const DisplayName = styled.div`
-    width: 185px;
-    height: 40px;
-    margin-left:20px;   
-    font-size: x-large;
-    font-weight: bold;
-`
+        width: 185px;
+        height: 40px;
+        margin-left:20px;   
+        font-size: x-large;
+        font-weight: bold;
+    `
 
 const EmailText = styled.div`
-    width: 185px;
-    height: 20px;
-    margin-top: 10px;
-    margin-left: -40px;
-`
+        width: 185px;
+        height: 20px;
+        margin-top: 10px;
+        margin-left: -40px;
+    `
 const EditProfile = styled.button`
-    position: absolute;
-    right: 100px;
-    width: 100px;
-    height: 30px;
-    background-color: #AB3E16;
-    border: 1px solid;
-    border-radius: 5px;
-    font-size: normal;
-    font-weight: bold;
-    color: #fff;
-`
+        position: absolute;
+        right: 100px;
+        width: 100px;
+        height: 30px;
+        background-color: #AB3E16;
+        border: 1px solid;
+        border-radius: 5px;
+        font-size: normal;
+        font-weight: bold;
+        color: #fff;
+    `
 
 const StatusBlock = styled.div`
-    width: 650px;
-    height: 160px;
-    size: 20px;
-    border-bottom: 1px solid #48120E;
-`
+        width: 650px;
+        height: 160px;
+        size: 20px;
+        border-bottom: 1px solid #48120E;
+    `
 const StatusText = styled.div`
-    margin-left:10px;
-    font-size: large;
-    font-weight: bold;
-`
+        margin-left:10px;
+        font-size: large;
+        font-weight: bold;
+    `
 
 const StatusStateBlock = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: center;
-    align-items: center;
-    justify-content: center;
-    width: 300px;
-    height: 120px;
-    border: 1px solid #48120E;
-    border-radius: 5px;
-`;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: center;
+        align-items: center;
+        justify-content: center;
+        width: 300px;
+        height: 120px;
+        border: 1px solid #48120E;
+        border-radius: 5px;
+    `;
 
 const QnAText = styled.div`
-    width: 130px;
-    margin-left:10px;
-`;
+        width: 140px;
+        margin-left:10px;
+        margin-right:-10px;
+    `;
 
 const StatusState = styled.div`
-    width: 130px;
-    height: 85px;
-    margin-left: 10px;
-    font-size: x-large;
-    font-weight: bold;
-`;
+        width: 130px;
+        height: 85px;
+        margin-left: 10px;
+        font-size: x-large;
+        font-weight: bold;
+    `;
 
 const QnABlock = styled.div`
-    display: flex;
-    width: 740px;
-    height: 220px;
-    margin-top: 20px;
-    flex-direction: column;
-`
+        display: flex;
+        width: 740px;
+        height: 220px;
+        margin-top: 20px;
+        flex-direction: column;
+    `
 
 const QnAList = styled.div`
-    width: 280px;
-    height: 130px;
-    margin-bottom: 20px;
-    border: 1px solid #48120E;
-    border-radius: 5px;
-    overflow-y: scroll;
-    padding: 0 8px 0 0;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: 100%;
-    &::-webkit-scrollbar {
-        width: 6px;
-    }
-    &::-webkit-scrollbar-track {
-        background-color: #BF8B67;
-        margin-right: 4px;
+        width: 280px;
+        height: 130px;
+        margin-bottom: 20px;
+        border: 1px solid #48120E;
         border-radius: 5px;
-    }
-    &::-webkit-scrollbar-thumb {
-        background-color: #AB3E16;
-        border-radius: 5px;
-    }
-    &::-webkit-scrollbar-thumb:hover {
-        background-color: #48120E;
-    }
+        overflow-y: scroll;
+        padding: 0 8px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        max-width: 100%;
+
     & > div {
-        margin-right: 4px;
-        padding-right: 10px;
-    }
-`
+    display: block;
+    margin-top: 5px;
+    border-radius: 5px;
+  }
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #BF8B67;
+    margin-top: 5px;
+    margin-right: 4px;
+    border-radius: 5px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #AB3E16;
+    border-radius: 5px;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #48120E;
+  }
+    `
 
 
 
@@ -167,32 +172,26 @@ const Mypage = () => {
     const getMemberInfo = async (member_id) => {
         try {
             const response = await axiosAuth2.get(`${process.env.REACT_APP_URL_NGROKTEST}/users/${member_id}`);
+            console.log('getMemberInfo response:', response);
             return response.data;
         } catch (err) {
             console.error(err);
             alert('회원 정보를 불러오는 도중 오류가 발생했습니다. 새로고침 후 다시 시도해주세요.');
         }
     }
-    // const [memberInfo, isPending, error] = useFetch(`${process.env.REACT_APP_URL_NGROKTEST}/users/${userInfo.memberId}`);
-    // const getMemberInfo = async (member_id) => {
-    //             axiosAuth2.get(`${process.env.REACT_APP_URL_NGROKTEST}/users/${member_id}`)
-    //             .then(res => res.data)
-    //             .catch(err => {console.log(err)})
-    // }
 
-    // userFullInfo.email
-    // userFullInfo.avatarLink
-    
     useEffect(() => {
         const fetchUserData = async () => {
+            console.log('fetchUserData called');
             const tempInfo = await getMemberInfo(userInfo.memberId);
+            console.log('fetched userFullInfo:', tempInfo);
             setUserFullInfo(tempInfo);
-            console.log(tempInfo);
+            console.log("setUserFullInfo", tempInfo);
         }
         fetchUserData();
-    
+
     }, [userInfo.memberId]);
-    
+
     const openEditProfile = () => {
         setIsEditProfileOpen(true);
     };
@@ -256,9 +255,27 @@ const Mypage = () => {
                             )}
                         </QnAList>
                     </div>
+                    <div>
+                        <QnAText>Deleted Questions</QnAText>
+                        <QnAList>
+                            {userFullInfo?.question?.length > 0 ? (
+                                userFullInfo?.question?.map((question, index) => (
+                                    <Link to={`/questions/${question.questionId}`} key={`question-${question.questionId || index}`}>
+                                        <div>{question.title}</div>
+                                    </Link>
+                                ))
+                            ) : (
+                                <div>No questions found.</div>
+                            )}
+                        </QnAList>
+                    </div>
                 </QnABlock>
             </MypageBlock>
-            <EditProfileModal isOpen={isEditProfileOpen} onRequestClose={closeEditProfile} userFullInfo={userFullInfo} />
+            <EditProfileModal 
+            isOpen={isEditProfileOpen} 
+            onRequestClose={closeEditProfile} 
+            userFullInfo={userFullInfo}
+            />
         </MypageWrap>
     )
 }
