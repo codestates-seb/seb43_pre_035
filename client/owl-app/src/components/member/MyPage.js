2b32746ff705519c8b482f0760bc3f0028357eb3
Gorden-Dosi <gosangmin1@gmail.com>
2023-04-27 21:56:59 +0900

    style : change mypage04/27,21:56

--- client/owl-app/src/components/member/MyPage.js
+++ client/owl-app/src/components/member/MyPage.js
@@ -4,11 +4,13 @@ import styled from "styled-components";
 import EditProfileModal from './EditProfileModal';
 import { UserContext } from '../../utils/UserContextConfig';
 import { axiosAuth2 } from '../../utils/axiosConfig';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBinoculars } from '@fortawesome/free-solid-svg-icons';
 // import useFetch from '../../utils/useFetch'; 
 
 const MypageWrap = styled.div`
         width: 100%;
-        height: 100vh;
+        height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -17,8 +19,8 @@ const MypageWrap = styled.div`
 const MypageBlock = styled.div`
         margin-top: 10px;
         padding: 20px;
-        height: 95vh;
-        width: 700px;
+        height: 90vh;
+        width: 70vh;
         background-color: #BF8B67;
         display: flex;
         flex-direction: column;
@@ -31,8 +33,8 @@ const UserBlock = styled.div`
         display: flex;
         flex-direction: center;
         margin-bottom:10px;
-        width: 700px;
-        height: 100px;
+        width: 65vh;
+        height: 11vh;
         align-items: center;
     `
 
@@ -46,40 +48,38 @@ const UserAvatar = styled.div`
     `
 
 const DisplayName = styled.div`
-        width: 185px;
-        height: 40px;
-        margin-left:20px;   
+        width: 8vh;
+        height: 2vh;
+        margin-left:2vh;   
         font-size: x-large;
         font-weight: bold;
     `
 
 const EmailText = styled.div`
-        width: 185px;
-        height: 20px;
-        margin-top: 10px;
-        margin-left: -40px;
+        width: 20vh;
+        height: 2vh;
+        margin-top: 2vh;
+        margin-left : 2vh ;
     `
 const EditProfile = styled.button`
-        position: absolute;
-        right: 100px;
-        width: 100px;
-        height: 30px;
+        width: 10vh;
+        height: 3vh;
+        margin-left: 1vh;
         background-color: #AB3E16;
         border: 1px solid;
         border-radius: 5px;
-        font-size: normal;
+        font-size: small;
         font-weight: bold;
         color: #fff;
     `
 
 const StatusBlock = styled.div`
-        width: 650px;
-        height: 160px;
-        size: 20px;
+        width: 65vh;
+        height: 16vh;
         border-bottom: 1px solid #48120E;
     `
 const StatusText = styled.div`
-        margin-left:10px;
+        margin-left:2vh;
         font-size: large;
         font-weight: bold;
     `
@@ -90,50 +90,49 @@ const StatusStateBlock = styled.div`
         flex-direction: center;
         align-items: center;
         justify-content: center;
-        width: 300px;
-        height: 120px;
+        width: 29vh;
+        height: 10vh;
         border: 1px solid #48120E;
         border-radius: 5px;
     `;
 
 const QnAText = styled.div`
-        width: 140px;
-        margin-left:10px;
-        margin-right:-10px;
+        font-size: normal;
+        font-weight: bold;
     `;
 
 const StatusState = styled.div`
-        width: 130px;
-        height: 85px;
-        margin-left: 10px;
+        width: 11vh;
+        height: 3vh;
+        margin-left: 1vh;
         font-size: x-large;
         font-weight: bold;
     `;
 
 const QnABlock = styled.div`
         display: flex;
-        width: 740px;
-        height: 220px;
+        width: 500vh;
+        height: 63vh;
         margin-top: 20px;
         flex-direction: column;
     `
 
 const QnAList = styled.div`
-        width: 280px;
-        height: 130px;
-        margin-bottom: 20px;
-        border: 1px solid #48120E;
-        border-radius: 5px;
-        overflow-y: scroll;
-        padding: 0 8px;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        max-width: 100%;
+  width: 60vh;
+  height: 15vh;
+  margin-bottom: 3vh;
+  border: 1px solid #48120E;
+  border-radius: 5px;
+  overflow-y: scroll;
+  padding: 0 8px;
+  font-size: small;
 
-    & > div {
-    display: block;
-    margin-top: 5px;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    margin: 5px;
     border-radius: 5px;
   }
 
@@ -153,7 +152,42 @@ const QnAList = styled.div`
   &::-webkit-scrollbar-thumb:hover {
     background-color: #48120E;
   }
-    `
+`;
+
+const QuestionRow = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  color: #fff;
+`;
+
+const QuestionInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  color: #fff;
+`;
+
+const CreatedDate = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 20%;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+    color: var(--colors-yellow);
+    padding-right: 6px;
+`;
+
+const ViewCount = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 20%;
+`;
 
 
 
@@ -219,8 +253,6 @@ const Mypage = () => {
                         <div>
                             <QnAText>Questions</QnAText>
                             <StatusState>{userFullInfo?.question?.length}</StatusState>
-                        </div>
-                        <div>
                             <QnAText>Answers</QnAText>
                             <StatusState>{userFullInfo?.answer?.length}</StatusState>
                         </div>
@@ -230,51 +262,72 @@ const Mypage = () => {
                     <div>
                         <QnAText>Questions</QnAText>
                         <QnAList>
-                            {userFullInfo?.question?.length > 0 ? (
-                                userFullInfo?.question?.map((question, index) => (
+                        {userFullInfo?.question?.length > 0 ? (
+                            userFullInfo?.question
+                                .filter((question) => question.questionStatus === "QUESTION_REGISTRATION")
+                                .map((question, index) => (
                                     <Link to={`/questions/${question.questionId}`} key={`question-${question.questionId || index}`}>
-                                        <div>{question.title}</div>
+                                        <div>
+                                            <QuestionRow>{question.title}</QuestionRow>
+                                            <CreatedDate>{new Date(question.createdDate).toLocaleDateString("ko-KR", { year: "numeric", month: "2-digit", day: "2-digit" })}</CreatedDate>
+                                            <StyledIcon icon={faBinoculars} />
+                                            <ViewCount>{question.viewCount}</ViewCount>
+                                        </div>
                                     </Link>
                                 ))
-                            ) : (
-                                <div>No questions found.</div>
-                            )}
+                        ) : (
+                            <div>No questions found.</div>
+                        )}
                         </QnAList>
                     </div>
                     <div>
                         <QnAText>Answers</QnAText>
                         <QnAList>
-                            {userFullInfo?.answer?.length > 0 ? (
-                                userFullInfo?.answer?.map((answer, index) => (
-                                    <Link to={`/questions/${answer.question.questionId}`} key={`answer-${answer.answerId || index}`}>
-                                        <div>{answer.content}</div>
-                                    </Link>
-                                ))
-                            ) : (
-                                <div>No answers found.</div>
-                            )}
-                        </QnAList>
+                        {userFullInfo?.answer?.length > 0 ? (
+                            userFullInfo?.answer?.map((answer, index) => (
+                                <Link to={`/questions/${answer.question.questionId}`} key={`answer-${answer.answerId || index}`}>
+                                    <div>
+                                        <QuestionRow>{answer.content}</QuestionRow>
+                                        <CreatedDate>{new Date(answer.createdDate).toLocaleDateString("ko-KR", { year: "numeric", month: "2-digit", day: "2-digit" })}</CreatedDate>
+                                        <StyledIcon icon={faBinoculars} />
+                                        <ViewCount>{answer.question.viewCount}</ViewCount>
+                                    </div>
+                                </Link>
+                            ))
+                        ) : (
+                            <div>No answers found.</div>
+                        )}
+                    </QnAList>
                     </div>
                     <div>
                         <QnAText>Deleted Questions</QnAText>
-                        <QnAList>
-                            {userFullInfo?.question?.length > 0 ? (
-                                userFullInfo?.question?.map((question, index) => (
+                            <QnAList>
+                        {userFullInfo?.question?.length > 0 ? (
+                            userFullInfo?.question
+                                .filter((question) => question.questionStatus === "QUESTION_DELETE")
+                                .map((question, index) => (
                                     <Link to={`/questions/${question.questionId}`} key={`question-${question.questionId || index}`}>
-                                        <div>{question.title}</div>
+                                        <span>
+                                            <QuestionRow>{question.title}</QuestionRow>
+                                            </span>
+                                            <span>
+                                            <CreatedDate>{new Date(question.createdDate).toLocaleDateString("ko-KR", { year: "numeric", month: "2-digit", day: "2-digit" })}</CreatedDate>
+                                            <StyledIcon icon={faBinoculars} />
+                                            <ViewCount>{question.viewCount}</ViewCount>
+                                        </span>
                                     </Link>
                                 ))
-                            ) : (
-                                <div>No questions found.</div>
-                            )}
+                        ) : (
+                            <div>No questions found.</div>
+                        )}
                         </QnAList>
                     </div>
                 </QnABlock>
             </MypageBlock>
-            <EditProfileModal 
-            isOpen={isEditProfileOpen} 
-            onRequestClose={closeEditProfile} 
-            userFullInfo={userFullInfo}
+            <EditProfileModal
+                isOpen={isEditProfileOpen}
+                onRequestClose={closeEditProfile}
+                userFullInfo={userFullInfo}
             />
         </MypageWrap>
     )
